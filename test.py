c5a2860189d87c322e3eafd950a3308ca96ee1ed
Kuldip Lakhtariya <kuldip2611lakhtariya@gmail.com>
2026-06-14 22:05:06 +0530

    Refactor according to flask API

--- test.py
+++ test.py
@@ -2,21 +2,19 @@ import requests
 
 url = "http://127.0.0.1:5000/predict"
 
-
 sample_data = {
-    "Gender": 1,
-    "age_group_order": 3,
-    "scheduled_hour": 14,
-    "Scholarship": 0,
-    "Hypertension": 1,
-    "Diabetes": 0,
-    "Alcoholism": 0,
-    "Handicap": 0,
-    "SMS_received": 1,
-    "wait_group_order": 2
+  "Age": 75,
+  "scheduled_date": "2026-01-15",
+  "scheduled_time": "23:00",
+  "appointment_date": "2026-06-15",
+  "Gender": 0,
+  "Scholarship": 0,
+  "Hypertension": 1,
+  "Diabetes": 0,
+  "Alcoholism": 0,
+  "Handicap": 0,
+  "SMS_received": 0
 }
 
 response = requests.post(url, json=sample_data)
 print(response.json())
-
-
